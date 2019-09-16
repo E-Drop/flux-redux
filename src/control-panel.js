cbcdd46ccb5eb94fe44f2466241e3f09c6080ace
E-Drop <erosedno.developer@gmail.com>
2019-09-16 14:34:01 +0200

    remove unnecesary code and add some comments trying to explain what is doing the code

--- src/control-panel.js
+++ src/control-panel.js
@@ -1,10 +1,15 @@
 import {Dispatcher} from './flux';
 import {Store} from './flux';
 
+// Create an instance of the class Dispatcher
 const controlPanelDispatcher = new Dispatcher();
+
+// Define action Types
 const UPDATE_USERMAME = 'UPDATE_USERMAME';
 const UPDATE_FONTSIZE = 'UPDATE_FONTSIZE';
 
+
+// Define Actions
 const userNameUpdateAction = (name) => {
     return {
         type: UPDATE_USERMAME,
@@ -19,7 +24,7 @@ const fontSizeUpdateAction = (name) => {
     }
 };
 
-
+// Add events listeners to fomrs fields to dispatch actions onchange
 document.getElementById('userNameInput').addEventListener('input', ({target}) => {
     const name = target.value;
     console.log("Dispatching...", name);
@@ -33,8 +38,10 @@ document.forms.fontSizeForm.fontSize.forEach(element => {
     })
 })
 
+// Create a class Store making an override of the abstracts methods defined in Store.js
 class UserPrefsStore extends Store {
     getInitialState() {
+        // check if local storage is empty to take static data of the specified object or take data from localStorage
         return localStorage['preferences'] ? JSON.parse(localStorage['preferences']) : {
             userName:'Edgar',
             fontSize: 'small',
@@ -46,10 +53,10 @@ class UserPrefsStore extends Store {
                 this.__state.userName = action.value;
                 this.__emitChange();
                 break;
-                case UPDATE_FONTSIZE:
-                        this.__state.fontSize = action.value;
-                        this.__emitChange();
-                        break;
+            case UPDATE_FONTSIZE:
+                    this.__state.fontSize = action.value;
+                    this.__emitChange();
+                    break;
         }
     }
     getUserPreferences() {
@@ -57,6 +64,7 @@ class UserPrefsStore extends Store {
     }
 }
 
+// Make an instance of the custom Store defined up and add a listener in state to listeng changes an update localstorage
 const userPrefsStore = new UserPrefsStore(controlPanelDispatcher);
 userPrefsStore.addListener((state) => {
     console.info('the current state is...', state);
@@ -64,14 +72,12 @@ userPrefsStore.addListener((state) => {
     localStorage['preferences'] = JSON.stringify(state);
 })
 
+// Function that update the value of the forms with the data in the state
 const render = ({userName, fontSize}) => {
     document.getElementById('userName').innerText = userName;
     document.getElementsByClassName('container')[0].style.fontSize = fontSize === 'small' ? '16px' : '24px';
     document.forms.fontSizeForm.fontSize.value = fontSize;
 }
 
-// controlPanelDispatcher.register(action => {
-//     console.info("Received action...", action);
-// })
-
+// Call the getUsersPreferences to fill data on start
 render(userPrefsStore.getUserPreferences());
